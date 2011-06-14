6e0ba7c9d009286430d50a75f16847fc2ef33f35
Gustavo <gustavo.alexandre@gmail.com>
2011-06-14 14:05:29 -0300

    Insert execution messages in examples

--- examples/execute_tests.m
+++ examples/execute_tests.m
@@ -1,6 +1,7 @@
 nexecutions=100;
 
-fprintf('Executing tests on GSO\n');
+fprintf('Executing tests on GSO.\n');
+fprintf('Number of runs: %d\n', nexecutions);
 
 results_f1=zeros(1,nexecutions);
 results_f2=zeros(1,nexecutions);
@@ -17,101 +18,115 @@ results_f16=zeros(1,nexecutions);
 results_f17=zeros(1,nexecutions);
 results_f18=zeros(1,nexecutions);
 
-fprintf('Executing f1...');
+fprintf('Executing f1...\n');
 for i=1:nexecutions
     eval_gso_f1;
     results_f1(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f1), std(results_f1));
 fprintf('Done\n');
 
-fprintf('Executing f2...');
+fprintf('Executing f2...\n');
 for i=1:nexecutions
 eval_gso_f2;
 results_f2(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f2), std(results_f2));
 fprintf('Done\n');
 
-fprintf('Executing f3...');
+fprintf('Executing f3...\n');
 for i=1:nexecutions
 eval_gso_f3;
 results_f3(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f3), std(results_f3));
 fprintf('Done\n');
 
-fprintf('Executing f4...');
+fprintf('Executing f4...\n');
 for i=1:nexecutions
 eval_gso_f4;
 results_f4(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f4), std(results_f4));
 fprintf('Done\n');
 
-fprintf('Executing f5...');
+fprintf('Executing f5...\n');
 for i=1:nexecutions
 eval_gso_f5;
 results_f5(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f5), std(results_f5));
 fprintf('Done\n');
 
-fprintf('Executing f6...');
+fprintf('Executing f6...\n');
 for i=1:nexecutions
 eval_gso_f6;
 results_f6(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f6), std(results_f6));
 fprintf('Done\n');
 
-fprintf('Executing f8...');
+fprintf('Executing f8...\n');
 for i=1:nexecutions
 eval_gso_f8;
 results_f8(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f8), std(results_f8));
 fprintf('Done\n');
 
-fprintf('Executing f9...');
+fprintf('Executing f9...\n');
 for i=1:nexecutions
 eval_gso_f9;
 results_f9(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f9), std(results_f9));
 fprintf('Done\n');
 
-fprintf('Executing f10...');
+fprintf('Executing f10...\n');
 for i=1:nexecutions
 eval_gso_f10;
 results_f10(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f10), std(results_f10));
 fprintf('Done\n');
 
-fprintf('Executing f11...');
+fprintf('Executing f11...\n');
 for i=1:nexecutions
 eval_gso_f11;
 results_f11(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f11), std(results_f11));
 fprintf('Done\n');
 
-fprintf('Executing f14...');
+fprintf('Executing f14...\n');
 for i=1:nexecutions
 eval_gso_f14;
 results_f14(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f14), std(results_f14));
 fprintf('Done\n');
 
-fprintf('Executing f16...');
+fprintf('Executing f16...\n');
 for i=1:nexecutions
 eval_gso_f16;
 results_f16(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f16), std(results_f16));
 fprintf('Done\n');
 
-fprintf('Executing f17...');
+fprintf('Executing f17...\n');
 for i=1:nexecutions
 eval_gso_f17;
 results_f17(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f17), std(results_f17));
 fprintf('Done\n');
 
-fprintf('Executing f18...');
+fprintf('Executing f18...\n');
 for i=1:nexecutions
 eval_gso_f18;
 results_f18(i)=min(fx);
 end
+fprintf('f1\tMédia: %f\tDesvio: %f\n', mean(results_f18), std(results_f18));
 fprintf('Done\n');
 fprintf('End of tests\n');
